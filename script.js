c4cef6ef6326574edfe1233d2c83216e11add095
heroinsabuser <maksim-kozlov-2002@mail.ru>
2023-08-21 15:55:51 +0300

    fix − and  ← bugs

--- script.js
+++ script.js
@@ -32,11 +32,11 @@ function handleSymbol(symbol){
             if(buffer.length ===1){
                 buffer = '0';
             }else{
-                buffer = buffer.toString(0, buffer.length - 1);
+                buffer = buffer.substring(0, buffer.length - 1);
             }
             break;
             case '+':
-            case '-':
+            case '−':
             case '×':
             case '÷':
                 handleMath(symbol);
@@ -63,7 +63,7 @@ function handleMath(symbol){
 function flushOperation(intBuffer){
     if(previousOperator === '+'){
         runningTotal += intBuffer;
-    }else if(previousOperator === '-'){
+    }else if(previousOperator === '−'){
         runningTotal -= intBuffer;
     }else if(previousOperator === '×'){
         runningTotal *= intBuffer;
